88160122f9b58a884ca55080f8c0988403cdf66e
NevesTi <ceelso.7@gmail.com>
2023-05-31 12:04:36 -0300

    trabalho feiro

--- App.tsx
+++ App.tsx
@@ -1,20 +1,59 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, Button, StyleSheet, TouchableHighlight } from 'react-native';
 
-export default function App() {
+const App = () => {
+  const [count, setCount] = useState(0);
+
+  const incrementCount = () => {
+    setCount(count + 1);
+  };
+
+  
+
   return (
+
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.text2}>Você clicou</Text>
+      <Text style={styles.text}>Contador: {count}</Text>
+      
+      <TouchableHighlight
+        style={styles.button} onPress={() => { incrementCount() }}>
+        <Text
+          style={{ color: '#fff', fontSize: 15 }}>
+          Clique aqui!
+        </Text>
+      </TouchableHighlight>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
   },
+  text: {
+    fontSize: 24,
+    marginBottom: 20,
+  },
+  text2:{
+fontSize: 24,
+margin:150
+
+  },
+  button: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    elevation: 4,
+    backgroundColor: '#6676f1',
+    width: 100,
+    height: 100,
+    borderRadius: 100,
+    position: 'absolute',
+    bottom: 50,
+  },
+  
 });
+
+export default App;
